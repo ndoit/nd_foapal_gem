8935892244ce072daadbf223d35801af69cd6fc2
Teresa Meyer <tmeyer2@nd.edu>
2018-01-24 04:08:30 +0000

    added javascript to handle orgn_data_input fields and example in the dummy spec.  Added documentation.

--- spec/dummy/config/routes.rb
+++ spec/dummy/config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   resources :precs
   root 'parent_records#index'
   get '/parent_records/new/:approval_flag(/:email_detail_cb_flag)', to: 'parent_records#new'
+  get '/limited_org_tests/:number_of_orgns', to: 'limited_org_tests#new'
   resources :parent_records
   resources :payroll_accounts_parent_records
+  resources :limited_org_tests
   mount NdFoapalGem::Engine => "/nd_foapal_gem"
 end
