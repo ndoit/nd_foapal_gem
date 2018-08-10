27589d63341b895bbab15f059445eae620080c9f
Kingdon Barrett <kingdon.b@nd.edu>
2018-08-10 14:27:31 -0400

    there is a regression between 0.2.0 and 0.2.8

--- lib/nd_foapal_gem/version.rb
+++ lib/nd_foapal_gem/version.rb
@@ -1,3 +1,8 @@
 module NdFoapalGem
+  # -  revision: 03c383bdaa538275f6710b12da59127aced47057
+  # +  revision: ae1a82a7b71b27fc4fad9abd9d14665df26699ac
+  #    specs:
+  # -    nd_foapal_gem (0.2.8)
+  # +    nd_foapal_gem (0.2.0)
   VERSION = "0.2.8"
 end
